85bd991210cf5a685f44d9ae8f75953714ab2e76
raj <chinnujune@gmail.com>
2018-08-08 00:00:56 +0530

    #Feature update for landing page

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -32,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem "twitter-bootstrap-rails"
+gem 'owlcarousel-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Casmoclap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+	config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+	config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
+	config.assets.enabled = true
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
