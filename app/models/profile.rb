c634e10910b4152240726e0c80f0a1a87b319119
Ratnakar <kezy485@gmail.com>
2019-02-21 18:37:44 +0530

    comment out has_attached file

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,12 +10,12 @@ class Profile < ApplicationRecord
 	accepts_nested_attributes_for :certificates, allow_destroy: true
 
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	# has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+ #  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :id_front_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :id_front_image, content_type: /\Aimage\/.*\z/
+ #  has_attached_file :id_front_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+ #  validates_attachment_content_type :id_front_image, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :id_back_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :id_back_image, content_type: /\Aimage\/.*\z/
+ #  has_attached_file :id_back_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+ #  validates_attachment_content_type :id_back_image, content_type: /\Aimage\/.*\z/
 end
